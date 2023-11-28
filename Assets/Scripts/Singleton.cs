8467fd9ef684844191c3325dd39c19370b2c19b6
Fcmz <2796744106@qq.com>
2023-11-28 14:39:31 +0800

    Add Singleton and MonoSingleton

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,63 +1,82 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Singleton<T> : MonoBehaviour where T : Component
+public abstract class Singleton<T> where T : Singleton<T>, new()
 {
-	
-	#region Fields
 
-	/// <summary>
-	/// The instance.
-	/// </summary>
-	private static T instance;
+    #region Fields
 
-	#endregion
+    /// <summary>
+    /// The instance.
+    /// </summary>
+    private static T instance;
 
-	#region Properties
+    #endregion
 
-	/// <summary>
-	/// Gets the instance.
-	/// </summary>
-	/// <value>The instance.</value>
-	public static T Instance
-	{
-		get
-		{
-			if ( instance == null )
-			{
-				instance = FindObjectOfType<T> ();
-				if ( instance == null )
-				{
-					GameObject obj = new GameObject ();
-					obj.name = typeof ( T ).Name;
-					instance = obj.AddComponent<T> ();
-				}
-			}
-			return instance;
-		}
-	}
+    #region Properties
 
-	#endregion
+    /// <summary>
+    /// Gets the instance.
+    /// </summary>
+    /// <value>The instance.</value>
+    public static T Instance
+    {
+        get
+        {
+            if (Instance == null)
+            {
+                //ensure that only one thread can execute
+                lock (typeof(T))
+                {
+                    if (Instance == null)
+                    {
+                        instance = new T();
+                        instance.Init();
+                    }
+                }
+            }
 
-	#region Methods
+            return instance;
+        }
+    }
 
-	/// <summary>
-	/// Use this for initialization.
-	/// </summary>
-	protected virtual void Awake ()
-	{
-		if ( instance == null )
-		{
-			instance = this as T;
-			DontDestroyOnLoad ( gameObject );
-		}
-		else
-		{
-			Destroy ( gameObject );
-		}
-	}
 
-	#endregion
-	
+    #endregion
+
+    #region Methods
+
+    public static void CreateInstance()
+    {
+        DestroyInstance();
+        instance = Instance;
+    }
+
+    public static void DestroyInstance()
+    {
+        if (instance == null) return;
+
+        instance.Clear();
+        instance = default(T);
+    }
+
+    protected void Init()
+    {
+        OnInit();
+    }
+
+    public void Clear()
+    {
+        OnClear();
+    }
+
+    protected virtual void OnInit()
+    {
+    }
+
+    protected virtual void OnClear()
+    {
+    }
+    #endregion
+
 }
